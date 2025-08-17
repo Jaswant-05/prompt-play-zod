e9f52010a2e9d2b77bddbab4d247723b6ca40114
Jaswant-05 <sandhusjaswant1@gmail.com>
2025-08-17 13:43:09 -0400

    changed zod version

--- src/user.ts
+++ src/user.ts
@@ -1,7 +1,8 @@
-import  { z } from "zod";
+import { z } from "zod";
+
 
 export const UserSchema = z.object({
-    username: z.email(),
+    username: z.string().email(),
     password: z.string()
         .min(8)  
         .regex(/[a-z]/, { message: "Add a lowercase letter" })
